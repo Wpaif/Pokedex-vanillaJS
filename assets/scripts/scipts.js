0ff08528f372d6a7ac9228180ff5e630cee40b8f
Wilian Ferreira <wpaife@gmail.com>
2021-04-20 22:29:32 -0300

    Modificações visuais finalizadas

--- assets/scripts/scipts.js
+++ assets/scripts/scipts.js
@@ -27,15 +27,17 @@ const createPokemonPromises = () => Array(TOSEEUNTIL).fill().map((_, index) => {
 const generateHTML = pokemons => pokemons.reduce((acumulator, { name, id, types }) => {
 
     const elementType = types.map((typeInfo) => typeInfo.type.name)
+
     acumulator +=
         `<li class="card ${elementType[0]} border-${elementType[1]}">
         <img class = "card-image" alt = "${name}" src = "https://pokeres.bastionbot.org/images/pokemon/${id}.png">
 
-        <h2 class = "card-tittle">${id}. ${name}</h2>
+        <h2 class = "card-tittle">${id}. ${name.charAt(0).toUpperCase() + name.slice(1)}</h2>
 
-        <p class="card-subititle">${elementType.join(' | ')}</p>
+        <p class="card-subititle">${elementType.join(' | ').toUpperCase()}</p>
 
         </li>`
+
     return acumulator
 }, '')
 
